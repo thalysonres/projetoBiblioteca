f3b51edf43d8444bd44a9c0b26165762bacdbff8
thalysonres <thalysonres@gmail.com>
2021-02-11 14:26:42 -0300

    Criado os arquivos iniciais de Web e Server do projeto

---
+++ server/src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/users', (request, response) => {
+    console.log(request.query);
+
+    const users = [
+        {name: 'Diego', age: 25},
+        {name: 'Vini', age: 21}
+    ];
+
+    return response.json(users);
+});
+
+app.listen(3333);
