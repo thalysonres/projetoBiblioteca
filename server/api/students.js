e1fbd6664b043df97112259d2310b00449de3820
wil-rs <wilianrsantos.pro@gmail.com>
2021-03-24 03:07:20 -0300

    add student password encryption

--- server/api/students.js
+++ server/api/students.js
@@ -1,27 +1,41 @@
+const bcrypt = require('bcrypt')
+
 module.exports = app => {
+
+    const getHash = (password, callback) => {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(password, salt, (err, hash) => callback(hash))
+        })
+    }
+
     const save = (req, res) => {
-        app.db('students')
-            .insert({
-                name: req.body.name, 
-                phone: req.body.phone,
-                street: req.body.street,
-                district: req.body.district,
-                city: req.body.city,
-                state: req.body.state,
-                cpf: req.body.cpf,
-                pass: req.body.pass,
-                birthDate: req.body.birthDate,
-                regDate: req.body.regDate,
-                employees_id: req.body.employees_id,
-            })
-        .then( _ => res.status(204).send() )
-        .catch( err => res.status(400).json(err) )
+
+        getHash(req.body.pass, hash => {
+            const password = hash
+
+            app.db('students')
+                .insert({
+                    name: req.body.name,
+                    phone: req.body.phone,
+                    street: req.body.street,
+                    district: req.body.district,
+                    city: req.body.city,
+                    state: req.body.state,
+                    cpf: req.body.cpf,
+                    pass: password,
+                    birthDate: req.body.birthDate,
+                    regDate: req.body.regDate,
+                    employees_id: req.body.employees_id,
+                })
+                .then(_ => res.status(204).send())
+                .catch(err => res.status(400).json({message: err, status: "um erro"}))
+        })
     }
 
     const list = (req, res) => {
         app.db('students')
             .select('*')
-            .then( esta => res.json(esta) )
+            .then(esta => res.json(esta))
             .catch(err => res.json(err))
     }
 
