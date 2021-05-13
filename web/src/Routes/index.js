481e43575a0d9079c5d7375bc2fc506dbb72efad
wil-rs <wilianrsantos.pro@gmail.com>
2021-05-13 09:47:48 -0300

    pastas

---
+++ web/src/Routes/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const CadastroStudent = props => {
+    return (
+        <div>
+            <h1>Criar as rotas normalmente esse é uma arquivo teste.</h1>
+        </div>
+    )
+}
