cbc06bc702e138d562e84048bc128a6a4ae63d58
wil-rs <wilianrsantos.pro@gmail.com>
2021-05-13 09:44:31 -0300

    pastas

---
+++ web/src/pages/Students/CadastroStudent/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const CadastroStudent = props => {
+    return (
+        <div>
+            <h1>Cadastro estudente</h1>
+        </div>
+    )
+}
---
+++ web/src/pages/Students/ListStudent/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ListStudent = props => {
+    return (
+        <div>
+            <h1>Listar estudente</h1>
+        </div>
+    )
+}
