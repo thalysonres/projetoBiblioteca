df7a9abd0c5fa6f8f2ed97c35f73ed43a3c5181e
Thalyson <thalysonres@gmail.com>
2021-05-17 22:20:04 -0300

    mudanças css

--- web/src/pages/Student/StudentList/index.js
+++ web/src/pages/Student/StudentList/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import bookPP from '../../../assets/images/book-pp.svg';
 import estudanteP from '../../../assets/images/icons/estudantes2.svg';
+import estudantePP from '../../../assets/images/icons/estudantes.svg';
+import autor from '../../../assets/images/icons/autores.svg';
+import livro from '../../../assets/images/icons/livros2.svg';
+import localidade from '../../../assets/images/icons/localidade.svg';
+import emprestimo from '../../../assets/images/icons/emprestimos.svg';
+import funcionario from '../../../assets/images/icons/funcionarios.svg';
 import editar from '../../../assets/images/icons/editar.svg';
 import excluir from '../../../assets/images/icons/excluir.svg';
 import './styles.css';
@@ -16,19 +22,34 @@ function StudentList() {
         </div>
         <div id="items-menu">
           <ul id="navigation">
-            <li class="students"><a href="#">Estudantes</a></li>
-            <li class="authors"><a href="#">Autores</a></li>
-            <li class="books"><a href="#">Livros</a></li>
-            <li class="localities"><a href="#">Localidades</a></li>
-            <li class="loans"><a href="#">Empréstimos</a></li>
-            <li class="employees"><a href="#">Funcionários</a></li>
+            <li class="students"><a href="#">
+              <img src={estudantePP} alt="estudantePP"/>Estudantes</a>
+            </li>
+            <li class="authors"><a href="#">
+              <img src={autor} alt="autor"/>Autores</a>
+            </li>
+            <li class="books"><a href="#">
+              <img src={livro} alt="livro"/>Livros</a>
+            </li>
+            <li class="localities"><a href="#">
+              <img src={localidade} alt="localidade"/>Localidades</a>
+            </li>
+            <li class="loans"><a href="#">
+              <img src={emprestimo} alt="empréstimo"/>Empréstimos</a>
+            </li>
+            <li class="employees"><a href="#">
+              <img src={funcionario} alt="funcionário"/>Funcionários</a>
+            </li>
           </ul>
         </div>
       </div>
       <div id="main">
         <div id="create">
-          <img src={estudanteP} alt="estudantes"/>
-          <span>Estudantes</span>
+          <div id="new">
+            <img src={estudanteP} alt="estudantes"/>
+            <span>Estudantes</span>
+          </div>
+          
         </div>
         <div id="list">
           <section class="allStudents">
