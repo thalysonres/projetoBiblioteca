82b536a664db3bfb8b6aab7e7b5931b453165d9d
Thalyson <thalysonres@gmail.com>
2021-06-04 16:28:34 -0300

    criação da view completa de um livro

--- web/src/pages/LiteraryWorks/LiteraryWorksInfo/index.js
+++ web/src/pages/LiteraryWorks/LiteraryWorksInfo/index.js
@@ -21,9 +21,57 @@ function LiteraryWorksInfo() {
           </div>
         </div>
 
-        <div id="literaryWorksI_list">
+        <div id="literaryWorkI_list">
           <section>
             <h2>Título do livro</h2>
+            <table>
+              <tbody>
+                <tr className="literaryWorkI_title">
+                  <th className="capa">Capa do livro</th>
+                  <th colspan="4">Autor</th>
+                </tr>
+                <tr className="literaryWorkI_list">
+                  <td rowspan="9">Capa do livro</td>
+                  <td colspan="4">Nome do autor</td>
+                </tr>
+                <tr className="literaryWorkI_title">
+                  <th colspan="3">Tradutor</th>
+                  <th>Edição</th>
+                </tr>
+                <tr className="literaryWorkI_list">
+                  <td colspan="3">Tradutor</td>
+                  <td>Nº</td>
+                </tr>
+                <tr className="literaryWorkI_title">
+                  <th>Páginas</th>
+                  <th>Ano</th>
+                  <th colspan="2">Editora</th>
+                </tr>
+                <tr className="literaryWorkI_list">
+                  <td>14</td>
+                  <td>2014</td>
+                  <td colspan="2">Panini</td>
+                </tr>
+                <tr className="literaryWorkI_title">
+                  <th colspan="2">ISBN</th>
+                  <th colspan="2">CDD</th>
+                </tr>
+                <tr className="literaryWorkI_list">
+                  <td colspan="2">ISBN</td>
+                  <td colspan="2">CDD</td>
+                </tr>
+                <tr className="literaryWorkI_title">
+                  <th>CDU</th>
+                  <th>Localidade</th>
+                  <th colspan="2">Local de Publicação</th>
+                </tr>
+                <tr className="literaryWorkI_list">
+                  <td>CDU</td>
+                  <td>Localidade</td>
+                  <td colspan="2">Local de Publicação</td>
+                </tr>
+              </tbody>
+            </table>
           </section>
         </div>
 
